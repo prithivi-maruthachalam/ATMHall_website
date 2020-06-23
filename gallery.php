4c06eb71869055c85f05f631266a6bf578a2484b
prythagoras <prithivimaruthachalam@gmail.com>
2020-06-23 21:26:50 +0530

    Solved gallery images-flexbox alignment issue

--- gallery.php
+++ gallery.php
@@ -44,7 +44,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-        <link rel="stylesheet" href="home_style.css">
+        <link rel="stylesheet" href="gallery_style.css">
         <link rel="stylesheet" href="static_styling.css">
         
         <title>ATM Hall | Home </title>
