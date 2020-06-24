fd099ca4cebc0151695d8867308608e74d24eab8
prythagoras <prithivimaruthachalam@gmail.com>
2020-06-24 09:08:21 +0530

    Gallery coloumn-wise content dynamism implemented

--- gallery.php
+++ gallery.php
@@ -1,34 +1,51 @@
 <?php
-    $imagesDir = "galleryImages/";
-    $images = scandir($imagesDir);
+    $imagesDir = "content/galleryImages/";
+    $subDirs = scandir($imagesDir);
     $totalImages = 0;
-    
-    //this is going to hold all our images
-    $imageObjects = array();
-    
-    foreach($images as $image){
-        if($image == ".." || $image == "."){
-            continue;
-        } else {
-            $imageObject = array();
-            $imageURL = "".$imagesDir.$image;
-            $imageObject = explode("_",$imageURL);
-            $imageObject[0] = (int)explode("/",$imageObject[0])[1];
-            $imageObject[2] = explode(".",$imageObject[2])[0];
-            $imageObject[3] = $imageURL;
-    
-            array_push($imageObjects,$imageObject);
-        }
-    }
 
-    //$imageObjects is the colelction of images
-    //the next 3 lines split the images into three chunks to arrange them into three coloumns for the flexbox
-    $chunkMaxSize = sizeof($imageObjects)/3;
-    $chunkMaxSize = ceil ($chunkMaxSize);
-    usort($imageObjects, function($a, $b) {
-        return $a[0] <=> $b[0];
-    });
-    $imageObjectsList = array_chunk($imageObjects,$chunkMaxSize);
+    //the final array for holding the 3 coloumns of images
+    $imageObjectsList = array();
+    
+    //go through subfolder for each coloumn
+    foreach($subDirs as $subDir){
+        if($subDir == ".." || $subDir == "."){
+            continue;
+        }
+        //getting the actual images in the folder
+        $images = scandir($imagesDir.$subDir);
+        //init arary for images in this sub-directory
+        $imageObjects = array();
+
+        foreach($images as $image){
+            if($image == ".." || $image == "."){
+                continue;
+            } else {
+                $imageObject = array();
+                $imageURL = "".$imagesDir.$subDir."/".$image;
+                $imageObject = explode("_",$imageURL);
+                $imageObject[0] = (int)explode("/",$imageObject[0])[1];
+                $imageObject[2] = explode(".",$imageObject[2])[0];
+                $imageObject[3] = $imageURL;
+                
+                array_push($imageObjects,$imageObject);
+            }
+        }
+
+        //sort the images array for the current sub-directory
+        usort($imageObjects, function($a, $b) {
+            return $a[0] <=> $b[0];
+        });
+
+        //add the images from the current sub-directory to the list of iamges for each coloumn
+        array_push($imageObjectsList,$imageObjects);
+    }
+    //this is going to hold all our images
+    
+    
+
+
+
+
     
     //$imageObjectsList contains 3 lists, which are by themselves, lists of our image objects
 
