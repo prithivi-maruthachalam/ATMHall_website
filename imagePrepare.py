91cbf7f67a6fa5e06b4d7b04a68bb0b9eea132ca
prythagoras <prithivimaruthachalam@gmail.com>
2020-06-29 09:44:35 +0530

    Updated file sizes and aspect ratios for images in documentation

--- imagePrepare.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for count,_ in enumerate(os.listdir("galleryImages")):
-    src = "galleryImages/" + _
-    dst = "galleryImages/" + str(count) + "_" + "some test text " + str(count) + "_" + "alt text for image " + str(count) + ".jpeg"
-    
-
-    os.rename(src,dst)
